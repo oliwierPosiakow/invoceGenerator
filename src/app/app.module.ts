c75ff2a3a6ac20fc96b3ac61fc4a373358f5c000
Oliwier Posiakow <oliwierposiek@gmail.com>
2023-07-28 12:38:06 +0200

    Fetching data from JSON file and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { InvoiceComponent } from './screens/invoice/invoice.component';
-import { ListaTowarowComponent } from './screens/lista-towarow/lista-towarow.component';
-import { MenuComponent } from './UI/menu/menu.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {InvoiceComponent} from './screens/invoice/invoice.component';
+import {ListaTowarowComponent} from './screens/lista-towarow/lista-towarow.component';
+import {MenuComponent} from './UI/menu/menu.component';
 import {MatIconModule} from "@angular/material/icon";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ListComponentComponent } from './UI/list-component/list-component.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ListComponentComponent} from './UI/list-component/list-component.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
--- src/app/screens/invoice/invoice.component.ts
+++ src/app/screens/invoice/invoice.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 
 @Component({
@@ -6,11 +6,33 @@ import {Router} from "@angular/router";
   templateUrl: './invoice.component.html',
   styleUrls: ['./invoice.component.scss']
 })
-export class InvoiceComponent {
+export class InvoiceComponent implements OnInit{
+  invoiceData: {id: string, name: string, count: number, price: number}[] = []
+  companyData: {name: string, address: string, phones: string[] } = {name: '', address: '', phones: []}
+  loading: boolean = false
   constructor(private router: Router) {
     // @ts-ignore
     this.invoiceData = this.router.getCurrentNavigation()?.extras.state
-    console.log(this.invoiceData)
   }
-  invoiceData: {id: string, name: string, count: number, price: number}[] = []
+  async getJsonData(){
+    await fetch('assets/company.json')
+      .then(res => res.json())
+      .then(data => {
+        this.companyData = data
+      })
+  }
+  ngOnInit():void {
+    this.loading = true
+    try {
+      if(this.invoiceData){
+        this.getJsonData()
+      }
+
+    }
+    catch (e){
+      console.log(e)
+    }
+    this.loading = false
+  }
 }
+
